c58c1665555e98daf2a33ad0124762ae26de5c12
Sufail Badusha <sufailbadushat@gmail.com>
2023-12-02 15:22:42 +0530

    first commit

---
+++ src/main/java/com/eous/events/BirthdayWishesApplication.java
@@ -0,0 +1,13 @@
+package com.eous.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayWishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayWishesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eous/events/BirthdayWishesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eous.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayWishesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
